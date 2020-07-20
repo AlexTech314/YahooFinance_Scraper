88ecc8dadf52afc3301d88ed5a3043e67846ae8f
AlexTech314 <alex.s@wustl.edu>
2020-07-20 16:04:43 -0400

    want to get plots going

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -2,43 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-def appleStockPriceTracker():
-
-    url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch'
+def tracker(url, name):
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml') 
     # print(soup)
-    price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    print('Current price of Apple Stock: ' + price)
+    # print(soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}))
+    try:
+        price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        print('Current price of ' + name + ' ' + price)
+    
+    except:
+        print("skip")
 
 
-def amazonStockPriceTracker():
-
-    url = 'https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml') 
-    # print(soup)
-    price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    print('Current price of Amazon Stock: ' + price)
-
-def nikolaStockPriceTracker():
-
-    url = 'https://finance.yahoo.com/quote/NKLA?p=NKLA&.tsrc=fin-srch'
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml') 
-    # print(soup)
-    price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    print('Current price of Nikola Corporation Stock: ' + price)
+while (True):
+   tracker('https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch', "Apple")
+   tracker('https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch', "Amazon")
+   tracker('https://finance.yahoo.com/quote/NKLA?p=NKLA&.tsrc=fin-srch', "Nikola Corporation")
+   tracker('https://finance.yahoo.com/quote/IMRN?p=IMRN&.tsrc=fin-srch', "Immuron Limited")
+   tracker('https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch', "Tesla")
+   tracker('https://finance.yahoo.com/quote/AZN?p=AZN&.tsrc=fin-srch', "AstraZeneca")
+   tracker('https://finance.yahoo.com/quote/GC=F?p=GC=F', "Gold")
+   tracker('https://finance.yahoo.com/quote/ACB', "Aurora Cannabis")
 
 
-count = 0
 
-while (count < 10):
-   appleStockPriceTracker()
-   amazonStockPriceTracker()
-   nikolaStockPriceTracker()
-   count = count + 1
+
+ 
+
+
